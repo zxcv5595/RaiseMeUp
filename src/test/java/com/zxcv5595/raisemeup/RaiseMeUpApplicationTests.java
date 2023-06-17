e8a93228c10e764ac0a786c9a8b822e88bd1aad8
zxcv5595 <zxcv5595@korea.ac.kr>
2023-06-18 01:36:34 +0900

    first commit

---
+++ src/test/java/com/zxcv5595/raisemeup/RaiseMeUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zxcv5595.raisemeup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaiseMeUpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
